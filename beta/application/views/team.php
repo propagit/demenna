245d0fd1c828cb9d9a00dba932ad82950087c0a2
Kaushtuv <kaushtuvgurung@gmail.com>
2015-02-04 17:57:03 +1100

    fleet work push

--- beta/application/views/team.php
+++ beta/application/views/team.php
@@ -32,6 +32,10 @@ DeMenna Cranes Team consists of specialist trained staff in:
             <li>DeMenna Cranes has a new Rail Division DMC Rail which supplies Heavy duty High rail crane trucks and EWPs</li>
         </ul>
         
+        <div class="linkedin">
+        	<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+			<script type="IN/MemberProfile" data-id="https://au.linkedin.com/pub/luke-demenna/66/246/380" data-format="inline" data-related="false"></script>
+        </div>
 
     </div>
 </div>
